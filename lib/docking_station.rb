6a2125f341a59c3f2466d3ba466a2b23e73e3871
Samed Yalniz <s.yalniz@hotmail.de>
2016-07-07 17:47:05 +0200

    Challenge 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,7 +3,8 @@ class Docking_station
   attr_reader :bike
 
   def release_bike
-      Bike.new
+      raise "No bikes available" unless @bike
+      @bike
   end
   def dock(bike)
     @bike = bike
